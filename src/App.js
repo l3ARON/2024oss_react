7251004dd71d9e41e737ac679619e834a1d7da2e
BARON <yuyoung4101@gmail.com>
2024-10-28 10:45:39 +0900

    edit App && add component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+// 만들어준 컴포넌트 추가
+import Component001 from './Component001';
 
 function App() {
-  return (
+  return (  // html 코드들을 리턴해줌
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div>
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          <p>20241028 react oss class</p>
+        </div>
+
+        <Component001/>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
+        click! move to react
       </header>
     </div>
   );
